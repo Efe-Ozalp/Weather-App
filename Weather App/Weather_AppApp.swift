e533c135ebe90fec0843fa064b039e2ce0031688
Efe Ozalp <efeozalp0101@gmail.com>
2023-09-06 11:14:53 -0500

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Efe Ozalp on 9/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
