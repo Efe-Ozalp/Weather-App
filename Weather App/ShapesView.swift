83f5b098ba57e38673c42e0aaae5c501dbf207c0
Efe Ozalp <efeozalp0101@gmail.com>
2023-09-08 10:16:01 -0500

    files set up

---
+++ Weather App/ShapesView.swift
@@ -0,0 +1,20 @@
+//
+//  ShapesView.swift
+//  Weather App
+//
+//  Created by Efe Ozalp on 9/8/23.
+//
+
+import SwiftUI
+
+struct ShapesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShapesView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShapesView()
+    }
+}
