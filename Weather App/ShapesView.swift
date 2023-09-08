c0d0f467f0e2a191319f7a6206dc99a6a3435adc
Efe Ozalp <efeozalp0101@gmail.com>
2023-09-08 11:13:51 -0500

    shapes

--- Weather App/ShapesView.swift
+++ Weather App/ShapesView.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 struct ShapesView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Circle()
+                
+                .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealWidth: 100, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealHeight: 100, maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                .background(.blue)
+            Rectangle()
+                .trim(from: 0, to: 0.5)
+                .background(.green)
+            Ellipse()
+                .stroke(.orange,lineWidth: 5)
+                .background(.brown)
+            Capsule()
+                .frame(width:300, height: 100)
+                .foregroundColor(.cyan)
+            RoundedRectangle(cornerRadius:10)
+                .frame(width:200, height:100)
+                .foregroundColor(.red)
+        }.ignoresSafeArea()
     }
 }
 
