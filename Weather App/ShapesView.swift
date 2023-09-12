8a036a45e762aa9ec21880144d31ed1a66a5d226
Efe Ozalp <efeozalp0101@gmail.com>
2023-09-12 10:57:52 -0500

    committing

--- Weather App/ShapesView.swift
+++ Weather App/ShapesView.swift
@@ -27,8 +27,18 @@ struct ShapesView: View {
                     .frame(width:200, height:100)
                     .foregroundColor(.red)
                 Rectangle()
-                    .size(width:100, height:100)
+                    .size(width:100,height:100)
                     .fill(.blue)
+                    .frame(width: 100, height:100, alignment: .center)
+                    .overlay(
+                        Rectangle()
+                            .stroke(Color.black, lineWidth: 25)
+                    )
+                    .padding()
+                RoundedRectangle(cornerRadius:10)
+                    .frame(width:300, height:50)
+                    .foregroundColor(.blue)
+                
             }.ignoresSafeArea()
         }
         
